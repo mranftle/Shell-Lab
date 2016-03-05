526e22d986d5e93eaa9948ddf4e1ca2efd432e45
mranftle <matthew.ranftle@gmil.com>
2016-03-05 14:05:52 -0600

    internal cmds

---
+++ project1/project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project1
+//
+//  Created by Matthew Ranftle on 3/5/16.
+//  Copyright © 2016 Matthew Ranftle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
